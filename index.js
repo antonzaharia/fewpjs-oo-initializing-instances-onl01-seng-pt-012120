af2aa1954399efe5a3ec776307d6f4afa93ef635
ANTON ZAHARIA <zaharia_anton@yahoo.com>
2020-08-08 14:20:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 // Write your code here
 class Breakfast {
   constructor(food, drink){
-    food = this.food
-    drink = this.drink
+    this.food = food
+    this.drink = drink
   }
 }
 class Lunch {
